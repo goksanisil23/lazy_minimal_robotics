6968ae998b9fce991e363bd0cf2fa0a8ac29dd7d
goxanisil23 <goxan.isil@gmail.com>
2023-01-25 11:36:49 +0100

    reformatting

--- KalmanFilter/kalman.hpp
+++ KalmanFilter/kalman.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include <iostream>
 #include <Eigen/Dense>
+#include <iostream>
 
-class KalmanFilter {
+class KalmanFilter
+{
 
-public:
-
+  public:
     // Generate the Kalman Filter with specified matrices
     // A : System dynamics matrix
     // B : Input matrix
@@ -14,30 +14,35 @@ public:
     // Q : Process noise covariance
     // R : Measurement noise covariance
     // P0 : Initial error covariance
-    KalmanFilter(
-    const Eigen::MatrixXd& A,
-    const Eigen::MatrixXd& B,
-    const Eigen::MatrixXd& C,
-    const Eigen::MatrixXd& Q,
-    const Eigen::MatrixXd& R,
-    const Eigen::MatrixXd& P0);
+    KalmanFilter(const Eigen::MatrixXd &A,
+                 const Eigen::MatrixXd &B,
+                 const Eigen::MatrixXd &C,
+                 const Eigen::MatrixXd &Q,
+                 const Eigen::MatrixXd &R,
+                 const Eigen::MatrixXd &P0);
 
     // Initialize the filter with a guess for the initial states
-    void init(const Eigen::VectorXd& x0);
+    void init(const Eigen::VectorXd &x0);
 
     // Update the a-priori estimates based on the system model and control input
-    void prediction_update(const Eigen::VectorXd& u);
+    void prediction_update(const Eigen::VectorXd &u);
 
     // Update the a-posteriori estimates based on the measurements
-    void innovation_update(const Eigen::VectorXd& y);
+    void innovation_update(const Eigen::VectorXd &y);
 
     // Return the current state
-    Eigen::VectorXd get_state() {return x_hat_post;};
+    Eigen::VectorXd get_state()
+    {
+        return x_hat_post;
+    };
 
     // Return the current state error covariance
-    Eigen::MatrixXd get_P() {return P_post;};    
+    Eigen::MatrixXd get_P()
+    {
+        return P_post;
+    };
 
-private:
+  private:
     // Matrices used in predict and correction steps
     Eigen::MatrixXd A, B, C, Q, R, P_prio, P_post, K;
 
@@ -55,5 +60,4 @@ private:
 
     // Estimated states
     Eigen::VectorXd x_hat_prio, x_hat_post;
-
 };
--- KalmanFilter/kalman_test.cpp
+++ KalmanFilter/kalman_test.cpp
@@ -7,33 +7,34 @@
  */
 
 #include <iostream>
-#include <vector>
-#include <random>
 #include <memory>
+#include <random>
+#include <vector>
 
 #include "kalman.hpp"
 
 #include "matplotlibcpp.h"
 
-int main() {
+int main()
+{
 
     // The real state value that we're trying to estimate
     double x_real = -0.37727;
     // To generate noisy measurements
-    double meas_noise_real_std_dev = 0.1;
-    std::default_random_engine meas_gen(0); // with a seed 0 for repeatibility
-    std::normal_distribution<double> meas_dist(x_real, meas_noise_real_std_dev);    
+    double                           meas_noise_real_std_dev = 0.1;
+    std::default_random_engine       meas_gen(0); // with a seed 0 for repeatibility
+    std::normal_distribution<double> meas_dist(x_real, meas_noise_real_std_dev);
 
     int n = 1; // number of states
     int m = 1; // number of measured variables
     int c = 1; // number of control input variables
 
-    Eigen::MatrixXd A(n,n); // system dynamics matrix
-    Eigen::MatrixXd B(n,c); // Input control matrix
-    Eigen::MatrixXd C(m,n); // Output matrix
-    Eigen::MatrixXd Q(n,n); // Process noise covariance
-    Eigen::MatrixXd R(m,m); // Measurement noise covariance
-    Eigen::MatrixXd P0(n,n); // Initial state estimate error covariance
+    Eigen::MatrixXd A(n, n);  // system dynamics matrix
+    Eigen::MatrixXd B(n, c);  // Input control matrix
+    Eigen::MatrixXd C(m, n);  // Output matrix
+    Eigen::MatrixXd Q(n, n);  // Process noise covariance
+    Eigen::MatrixXd R(m, m);  // Measurement noise covariance
+    Eigen::MatrixXd P0(n, n); // Initial state estimate error covariance
 
     // Trying to estimate the variable that we're directly measuring with some noise
     A << 1;
@@ -46,7 +47,7 @@ int main() {
     R << (meas_noise_real_std_dev) * (meas_noise_real_std_dev); // take "true" measurement error variance
 
     // Create the Kalman Filter
-    KalmanFilter kalman_f(A,B,C,Q,R,P0);
+    KalmanFilter kalman_f(A, B, C, Q, R, P0);
 
     // Some initial guess for the state we're trying to estimate
     Eigen::VectorXd x0(n);
@@ -61,11 +62,12 @@ int main() {
     std::vector<double> x_buffer;
     std::vector<double> x_hat_buffer;
     std::vector<double> P_buffer;
-    matplotlibcpp::figure_size(600,400);
+    matplotlibcpp::figure_size(600, 400);
 
-    for(int i = 0; i < 200; i++) {
+    for (int i = 0; i < 200; i++)
+    {
         y << meas_dist(meas_gen); // generate noisy measurement
-        u << 0; // No control input in this case
+        u << 0;                   // No control input in this case
         kalman_f.prediction_update(u);
         kalman_f.innovation_update(y);
 
@@ -75,25 +77,23 @@ int main() {
         y_buffer.push_back(y(0));
         P_buffer.push_back(kalman_f.get_P()(0));
     }
-        // Rendering
-        matplotlibcpp::clf();
-        matplotlibcpp::subplot(1, 2, 1);
-        matplotlibcpp::named_plot("x hat", x_hat_buffer);
-        matplotlibcpp::named_plot("measurement", y_buffer, ".r");
-        matplotlibcpp::named_plot("x true", x_buffer, "y");
-        matplotlibcpp::grid(true);
-        matplotlibcpp::legend();
-        matplotlibcpp::ylabel("State variable unit");
-        matplotlibcpp::xlabel("iterations");
-        matplotlibcpp::subplot(1, 2, 2);
-        matplotlibcpp::named_plot("P", P_buffer);
-        matplotlibcpp::grid(true);
-        matplotlibcpp::legend();
-        matplotlibcpp::ylabel("State est. err. covariance");
-        matplotlibcpp::xlabel("iterations");        
-        matplotlibcpp::pause(0);   
-
-
+    // Rendering
+    matplotlibcpp::clf();
+    matplotlibcpp::subplot(1, 2, 1);
+    matplotlibcpp::named_plot("x hat", x_hat_buffer);
+    matplotlibcpp::named_plot("measurement", y_buffer, ".r");
+    matplotlibcpp::named_plot("x true", x_buffer, "y");
+    matplotlibcpp::grid(true);
+    matplotlibcpp::legend();
+    matplotlibcpp::ylabel("State variable unit");
+    matplotlibcpp::xlabel("iterations");
+    matplotlibcpp::subplot(1, 2, 2);
+    matplotlibcpp::named_plot("P", P_buffer);
+    matplotlibcpp::grid(true);
+    matplotlibcpp::legend();
+    matplotlibcpp::ylabel("State est. err. covariance");
+    matplotlibcpp::xlabel("iterations");
+    matplotlibcpp::pause(0);
 
     return 0;
 }
