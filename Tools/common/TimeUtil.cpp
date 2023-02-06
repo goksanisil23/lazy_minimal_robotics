d765c2548bd86ecc3bf1fe419caa55733b241929
goxanisil23 <goxan.isil@gmail.com>
2023-02-06 19:23:15 +0100

    change float to double

--- Tools/common/TimeUtil.cpp
+++ Tools/common/TimeUtil.cpp
@@ -3,14 +3,14 @@
 namespace time_util
 {
 
-float getTimeDuration(const time_point &t2, const time_point &t1)
+double getTimeDuration(const time_point &t2, const time_point &t1)
 {
-    return std::chrono::duration<float, std::chrono::seconds::period>(t2 - t1).count();
+    return std::chrono::duration<double, std::chrono::seconds::period>(t2 - t1).count();
 }
 
 void showTimeDuration(const time_point &t2, const time_point &t1, const std::string &message)
 {
-    std::cout << message << std::chrono::duration<float, std::chrono::seconds::period>(t2 - t1).count() << std::endl;
+    std::cout << message << std::chrono::duration<double, std::chrono::seconds::period>(t2 - t1).count() << std::endl;
 }
 
 } // namespace time_util
--- Tools/common/TimeUtil.h
+++ Tools/common/TimeUtil.h
@@ -9,7 +9,7 @@ namespace time_util
 using time_point                 = decltype(std::chrono::high_resolution_clock::now());
 inline constexpr auto &chronoNow = std::chrono::high_resolution_clock::now;
 
-void  showTimeDuration(const time_point &t2, const time_point &t1, const std::string &message);
-float getTimeDuration(const time_point &t2, const time_point &t1);
+void   showTimeDuration(const time_point &t2, const time_point &t1, const std::string &message);
+double getTimeDuration(const time_point &t2, const time_point &t1);
 
 } // namespace time_util
